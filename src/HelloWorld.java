c85dc2c4e502d410a6c2d646629ca74b61d85f60
jtax <jonathan@jtaxdesign.co.uk>
2016-02-03 22:46:09 +0000

    Project Initialisation

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Created by bairdjb on 03/02/2016.
+ */
+public class HelloWorld {
+    private static String message = "HelloWorld";
+
+    public static void main(String[] args){
+        System.out.println(message);
+    }
+}
