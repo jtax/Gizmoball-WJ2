dd362625d0bde3223358192277fc604fec7d827f
Jonathan Baird <jonathan@jtaxdesign.co.uk>
2016-02-07 15:50:07 +0000

    Fleshing out model classes with basic methods as per class diagram

--- src/model/Components/Line.java
+++ src/model/Components/Line.java
@@ -1,9 +1,16 @@
 package model.Components;
 
 import model.Component;
+import model.Coordinate;
 
 /**
  * Created by baird on 06/02/2016.
  */
 public class Line implements Component {
+    Coordinate start, end;
+
+    public Line(int startX, int startY, int endX, int endY){
+        start = new Coordinate(startX, startY);
+        end = new Coordinate(endX, endY);
+    }
 }
