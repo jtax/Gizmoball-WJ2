9c958d2d2bbca72e058559c1cfbee0a421887604
Jonathan Baird <jonathan@jtaxdesign.co.uk>
2016-02-07 15:34:04 +0000

    Began writing basic methods required in the Coordinate and Gizmo classes

--- src/model/Coordinate.java
+++ src/model/Coordinate.java
@@ -4,4 +4,30 @@ package model;
  * Created by baird on 06/02/2016.
  */
 public class Coordinate {
+    int x, y;
+
+    public Coordinate(int x, int y){
+        setX(x);
+        setY(y);
+    }
+
+    public int getX(){
+        return x;
+    }
+
+    public int getY(){
+        return y;
+    }
+
+    public void setX(int x){
+        this.x = x;
+    }
+
+    public void setY(int y){
+        this.y = y;
+    }
+
+    public boolean equals(Coordinate coord){
+        return coord.getX() == this.getX() && coord.getY() == this.getY();
+    }
 }
