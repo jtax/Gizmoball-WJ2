0465240c31b1e048e0371a9eafafce55ad1cfaa1
Jonathan Baird <jonathan@jtaxdesign.co.uk>
2016-02-07 15:50:07 +0000

    Fleshing out model classes with basic methods as per class diagram

--- src/model/Ball.java
+++ src/model/Ball.java
@@ -1,7 +1,16 @@
 package model;
 
+import model.Components.Point;
+
 /**
  * Created by baird on 06/02/2016.
  */
 public class Ball {
+    Component point;
+    Coordinate velocity;
+
+    public Ball(int x, int y, int velocityX, int velocityY, int width){
+        point = new Point(x,y,width);
+        velocity = new Coordinate(velocityX,velocityY);
+    }
 }
--- src/model/Component.java
+++ src/model/Component.java
@@ -4,4 +4,5 @@ package model;
  * Created by baird on 06/02/2016.
  */
 public interface Component {
+
 }
