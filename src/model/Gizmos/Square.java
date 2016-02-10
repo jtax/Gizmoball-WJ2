33434e1139c55c0607912e27deb6ca66298c6051
Laurynas Sakalauskas <laurynas@helix.lt>
2016-02-10 11:55:58 +0000

    merged all branches

--- src/model/Gizmos/Square.java
+++ src/model/Gizmos/Square.java
@@ -1,15 +1,44 @@
 package model.Gizmos;
 
+import model.Component;
+import model.Components.Line;
+import model.Coordinate;
 import model.Gizmo;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by baird on 06/02/2016.
  */
 public class Square extends Gizmo {
 
-	@Override
-	public void rotate() {
-		// TODO Auto-generated method stub
-		
-	}
+    private int reflectionCoefficient = 1;
+
+    public Square(Coordinate origin) {
+        super(origin);
+    }
+
+    @Override
+    protected List<Component> calculateComponents() {
+        Coordinate origin = super.getOriginCoordinate();
+        double x = origin.getX();
+        double y = origin.getY();
+        Component top = new Line(x,y, x+1,y);
+        Component right = new Line(x+1,y, x+1,y-1);
+        Component bottom = new Line(x,y-1, x+1,y-1);
+        Component left = new Line(x,y, x,y-1);
+        return Arrays.asList(top,right,bottom,left);
+    }
+
+    public void rotate(){
+        //Pointless for Square: do nothing
+    }
+
+    public Coordinate calculateBound(){
+        Coordinate bound = super.getOriginCoordinate();
+        bound.setX(bound.getX()+1);
+        bound.setY(bound.getX()-1);
+        return bound;
+    }
 }
