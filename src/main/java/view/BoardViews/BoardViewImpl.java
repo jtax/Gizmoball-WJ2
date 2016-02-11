dd55fbfd7f9485b5a6d3caec32b200ecfca0d062
Christopher Bean <git@cbean.net>
2016-02-11 14:31:35 +0000

    Neatified the BoardViewImpl and fixed a size issue in GizmoBallView.

--- src/main/java/view/BoardViews/BoardViewImpl.java
+++ src/main/java/view/BoardViews/BoardViewImpl.java
@@ -5,6 +5,10 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
+import java.awt.Shape;
+import java.awt.geom.Ellipse2D;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -22,6 +26,7 @@ public class BoardViewImpl implements BoardView, Observer {
 	private Board board;
 	private JPanel panel;
 	private Mode mode;
+	private Collection<Shape> shapes;
 
 	public BoardViewImpl(Board board) {
 		setBoard(board);
@@ -29,10 +34,15 @@ public class BoardViewImpl implements BoardView, Observer {
 		panel = getDefaultLayout();
 		panel.setPreferredSize(new Dimension(500, 500));
 		panel.setBackground(Color.WHITE);
-		
+
 		mode = Mode.BUILD;
+		
+		shapes = new HashSet<Shape>();
+
+		// TODO: remove this bit of test code.
+		shapes.add(new Ellipse2D.Double(0, 0, 25, 25));
 	}
-	
+
 	private JPanel getDefaultLayout() {
 		return new JPanel() {
 			private static final long serialVersionUID = 1L;
@@ -42,11 +52,13 @@ public class BoardViewImpl implements BoardView, Observer {
 				super.paintComponent(g);
 
 				if (mode == Mode.BUILD)
-					drawGrid((Graphics2D) g, getHorizontalScalingFactor(), getVerticalScalingFactor());
+					drawGrid((Graphics2D) g);
+
+				drawShapes((Graphics2D) g);
 			}
 		};
 	}
-	
+
 	@Override
 	public JPanel getPanel() {
 		return panel;
@@ -56,15 +68,15 @@ public class BoardViewImpl implements BoardView, Observer {
 	public void update(Observable o, Object arg) {
 		panel.repaint();
 	}
-	
+
 	public Mode getMode() {
 		return mode;
 	}
-	
+
 	public void setMode(Mode mode) {
 		this.mode = mode;
 	}
-	
+
 	/**
 	 * Toggle run/build mode and return the new mode.
 	 * 
@@ -78,18 +90,25 @@ public class BoardViewImpl implements BoardView, Observer {
 		this.board = board;
 	}
 
-	private void drawGrid(Graphics2D g, int gridWidth, int gridHeight) {
+	private void drawShapes(Graphics2D g) {
+		for (Shape s : shapes)
+			g.draw(s);
+	}
+
+	private void drawGrid(Graphics2D g) {
 		for (int x = 0; x < board.getWidth(); x++)
-			drawColumn(g, x, gridWidth, gridHeight);
+			drawColumn(g, x);
 	}
 
-	private void drawColumn(Graphics2D g, int x, int gridWidth, int gridHeight) {
+	private void drawColumn(Graphics2D g, int x) {
 		for (int y = 0; y < board.getHeight(); y++)
-			drawGridSquare(g, x, y, gridWidth, gridHeight);
+			drawGridSquare(g, x, y);
 	}
 
-	private void drawGridSquare(Graphics2D g, int x, int y, int w, int h) {
-		Rectangle square = new Rectangle(x * w, y * h, w, h);
+	private void drawGridSquare(Graphics2D g, int x, int y) {
+		int width = getHorizontalScalingFactor(), height = getVerticalScalingFactor();
+
+		Rectangle square = new Rectangle(x * width, y * height, width, height);
 		g.draw(square);
 	}
 
